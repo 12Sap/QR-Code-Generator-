464bf09514b3219ad378816edd6763d219b9ce6b
12Sap <155049834+12Sap@users.noreply.github.com>
2024-10-05 10:48:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let inp = document.querySelector('.inp');
+
+let srchBtn = document.querySelector('.searchBtn');
+
+let outputBox = document.querySelector('.outputCont');
+
+let outImg = document.querySelector('.outImg');
+
+let dwnldImg = document.querySelector('.dwnldImg');
+outputBox.style.display = 'none';
+
+async function fetchQR(text) {
+ let qrUrl = `https://quickchart.io/qr?text=${text}`;
+ let data = await fetch(qrUrl);
+ return data.url;
+}
+
+srchBtn.addEventListener('click',async ()=>{
+ let txt = inp.value;
+ if (txt!='') {
+  let src = await fetchQR(txt);
+  outImg.setAttribute('src',src);
+  outputBox.style.display = 'flex';
+  dwnldImg.setAttribute('href',src);
+ } else {
+  outputBox.style.display = 'none';
+ }
+ txt = '';
+ 
+})
